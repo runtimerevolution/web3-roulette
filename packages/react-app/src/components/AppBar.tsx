a18763476683527ed937af9006dd23c519d45ba4
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-05-12 10:31:09 +0100

    Cleanup app bar component

--- packages/react-app/src/components/AppBar.tsx
+++ packages/react-app/src/components/AppBar.tsx
@@ -1,16 +1,17 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import MenuItem from '@mui/material/MenuItem';
-
+import { useState } from 'react';
+import {
+  Avatar,
+  Box,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Typography,
+} from '@mui/material';
 import logo from './../assets/Logo.svg';
 
 function ResponsiveAppBar() {
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
 
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
@@ -21,18 +22,23 @@ function ResponsiveAppBar() {
   };
 
   return (
-    <Container maxWidth={false} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: '1rem', backgroundColor: 'white' }} >
-      <Box sx={{ width: '15rem', ml: '5rem' }} component="a"
-        href="/">
-        <img src={logo} alt='Logo' />
+    <Container
+      maxWidth={false}
+      className="app-bar-container"
+      sx={{
+        display: 'flex',
+      }}
+    >
+      <Box className="nav-logo" component="a" href="/">
+        <img src={logo} alt="Logo" />
       </Box>
-      <Box sx={{ flexGrow: 0, mr: '5rem' }}>
-        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+      <Box className="profile-menu-container">
+        <IconButton onClick={handleOpenUserMenu}>
           <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
         </IconButton>
         <Menu
-          sx={{ mt: '45px' }}
           id="menu-appbar"
+          className="mt-40"
           anchorEl={anchorElUser}
           anchorOrigin={{
             vertical: 'top',
@@ -46,7 +52,7 @@ function ResponsiveAppBar() {
           open={Boolean(anchorElUser)}
           onClose={handleCloseUserMenu}
         >
-          <MenuItem key='Logout' onClick={handleCloseUserMenu}>
+          <MenuItem key="Logout" onClick={handleCloseUserMenu}>
             <Typography textAlign="center">Logout</Typography>
           </MenuItem>
         </Menu>
