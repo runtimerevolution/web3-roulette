d2924bd3c42e14c52320eb0cf9437e3be81120a7
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-05-12 17:10:31 +0100

    Format file

--- packages/react-app/src/components/GiveawayCard.tsx
+++ packages/react-app/src/components/GiveawayCard.tsx
@@ -1,7 +1,15 @@
-import { Card, CardMedia, CardContent, Typography, Box, Button, Skeleton } from "@mui/material";
-import { Giveaway } from "../lib/types";
-import { format } from "date-fns";
-import { useNavigate } from "react-router-dom";
+import { format } from 'date-fns';
+import { useNavigate } from 'react-router-dom';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Box,
+  Button,
+  Skeleton,
+} from '@mui/material';
+import { Giveaway } from '../lib/types';
 
 const GiveawayCard = (props: Giveaway) => {
   const navigate = useNavigate();
@@ -11,23 +19,39 @@ const GiveawayCard = (props: Giveaway) => {
       <CardMedia
         component="img"
         height="120"
-        image={props.image ? props.image : '/static/images/placeholder-image.jpg'}
+        image={
+          props.image ? props.image : '/static/images/placeholder-image.jpg'
+        }
       />
       <CardContent>
         <Typography gutterBottom variant="h5">
           {props.title}
         </Typography>
-        <Typography gutterBottom>
-          {props.description}
-        </Typography>
+        <Typography gutterBottom>{props.description}</Typography>
         <Typography gutterBottom>
           <>🏆 {props.prize}</>
         </Typography>
         <Typography gutterBottom>
           <>🗓️ {format(props.endTime, 'MMMM d, yyyy')}</>
         </Typography>
-        <Box sx={{ mt: '1.5rem', borderRadius: '0.6rem', borderColor: '#6D6DF0', borderWidth: '3px', borderStyle: 'solid' }}>
-          <Button onClick={() => navigate(`/edit/${props.id}`)} sx={{ textTransform: 'capitalize', color: '#6D6DF0', fontWeight: 600, width: '100%' }}>
+        <Box
+          sx={{
+            mt: '1.5rem',
+            borderRadius: '0.6rem',
+            borderColor: '#6D6DF0',
+            borderWidth: '3px',
+            borderStyle: 'solid',
+          }}
+        >
+          <Button
+            onClick={() => navigate(`/edit/${props.id}`)}
+            sx={{
+              textTransform: 'capitalize',
+              color: '#6D6DF0',
+              fontWeight: 600,
+              width: '100%',
+            }}
+          >
             Manage
           </Button>
         </Box>
@@ -36,9 +60,13 @@ const GiveawayCard = (props: Giveaway) => {
   );
 };
 
-export const GiveawayCardSkeleton = () =>
+export const GiveawayCardSkeleton = () => (
   <Box>
-    <Skeleton variant="rectangular" height={120} sx={{ borderTopLeftRadius: '1.2rem', borderTopRightRadius: '1.2rem' }} />
+    <Skeleton
+      variant="rectangular"
+      height={120}
+      sx={{ borderTopLeftRadius: '1.2rem', borderTopRightRadius: '1.2rem' }}
+    />
     <Skeleton />
     <Skeleton />
     <Skeleton width={'60%'} />
@@ -51,5 +79,6 @@ export const GiveawayCardSkeleton = () =>
       <Skeleton width={'40%'} />
     </Box>
   </Box>
+);
 
 export default GiveawayCard;
