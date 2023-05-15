ceaf5e99e82074ada26fe6f3a7bdbf624b34abdb
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-05-15 12:06:19 +0100

    Add giveaway details page static UI

--- packages/react-app/src/pages/details.tsx
+++ packages/react-app/src/pages/details.tsx
@@ -1,18 +1,52 @@
-import DuckdartClient from '../services/backend';
+import { format } from 'date-fns';
+import { useLoaderData } from 'react-router-dom';
+
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { Stack, Typography } from '@mui/material';
+
 import { Giveaway } from '../lib/types';
+import DuckdartClient from '../services/backend';
 
 const loader = async ({ params }: any) => {
   const giveaway = (await DuckdartClient.getActiveGiveaways()).find(
-    (giveaway: Giveaway) => giveaway.id === params.giveawayId
+    (giveaway: Giveaway) => giveaway.id === Number(params.giveawayId)
   );
   if (!giveaway) {
     throw new Response('', { status: 404, statusText: 'Giveaway not found.' });
   }
-  return { giveaway };
+  return giveaway;
 };
 
 const GiveawayDetailsPage = () => {
-  return <div>Details of the giveaway</div>;
+  const giveaway = useLoaderData() as Giveaway;
+
+  return (
+    <>
+      <Stack direction="row">
+        <ChevronLeftIcon />
+        <Typography>Back</Typography>
+      </Stack>
+      <Stack direction="row">
+        <div>
+          <Typography>{giveaway.title}</Typography>
+          <img src={giveaway.image} alt={giveaway.title} />
+          <Typography>{giveaway.description}</Typography>
+          <Typography>
+            <>🏆 {giveaway.prize}</>
+          </Typography>
+          <Typography>
+            <>🗓️ {format(giveaway.endTime, 'MMMM d, yyyy')}</>
+          </Typography>
+        </div>
+        {giveaway.rules && (
+          <div>
+            <Typography>Rules</Typography>
+            <Typography>{giveaway.rules}</Typography>
+          </div>
+        )}
+      </Stack>
+    </>
+  );
 };
 
 export default GiveawayDetailsPage;
