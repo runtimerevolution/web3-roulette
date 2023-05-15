cbaf5da346d5ca8a68760ea3a52a0458b2261e95
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-05-15 15:01:37 +0100

    Update img fit and back btn space

--- packages/react-app/src/pages/details.tsx
+++ packages/react-app/src/pages/details.tsx
@@ -21,81 +21,83 @@ const GiveawayDetailsPage = () => {
   const giveaway = useLoaderData() as Giveaway;
 
   return (
-    <Stack direction="row" justifyContent="center" spacing="120px" mt="26px">
-      <div>
-        <Stack direction="row" spacing="10px" alignItems="center">
-          <ChevronLeftIcon sx={{ fontSize: '1.5rem' }} />
-          <Typography sx={{ fontSize: '18px' }}>Back</Typography>
-        </Stack>
-        <Typography
-          sx={{
-            fontSize: '35px',
-            fontWeight: 'bold',
-            color: '#303136',
-            marginTop: '13px',
-          }}
-        >
-          {giveaway.title}
-        </Typography>
-        <img
-          className="giveaway-img"
-          src={giveaway.image}
-          alt={giveaway.title}
-        />
-        <Typography
-          sx={{
-            fontSize: '16px',
-            color: '#303136',
-            width: '580px',
-            marginTop: '20px',
-          }}
-        >
-          {giveaway.description}
-        </Typography>
-        <Stack mt="14px" spacing="12px">
-          <Typography sx={{ fontSize: '18px', color: '#303136' }}>
-            <>
-              <span
-                className="giveaway-info-icon"
-                role="img"
-                aria-label="trophy"
-              >
-                🏆
-              </span>{' '}
-              {giveaway.prize}
-            </>
-          </Typography>
-          <Typography sx={{ fontSize: '18px', color: '#303136' }}>
-            <>
-              <span
-                className="giveaway-info-icon"
-                role="img"
-                aria-label="calendar"
-              >
-                🗓️
-              </span>{' '}
-              {format(giveaway.endTime, 'MMMM d, yyyy')}
-            </>
-          </Typography>
-        </Stack>
-      </div>
-      {giveaway.rules && (
-        <div className="giveaway-aside-info">
+    <>
+      <Stack direction="row" spacing="10px" alignItems="center" ml="60px">
+        <ChevronLeftIcon sx={{ fontSize: '1.5rem' }} />
+        <Typography sx={{ fontSize: '18px' }}>Back</Typography>
+      </Stack>
+      <Stack direction="row" justifyContent="center" spacing="120px" mt="26px">
+        <div>
           <Typography
             sx={{
-              fontSize: '20px',
+              fontSize: '35px',
               fontWeight: 'bold',
-              color: '#282655',
+              color: '#303136',
+              marginTop: '13px',
             }}
           >
-            Rules
+            {giveaway.title}
           </Typography>
-          <Typography sx={{ fontSize: '16px', marginTop: '5px' }}>
-            {giveaway.rules}
+          <img
+            className="giveaway-img"
+            src={giveaway.image}
+            alt={giveaway.title}
+          />
+          <Typography
+            sx={{
+              fontSize: '16px',
+              color: '#303136',
+              width: '580px',
+              marginTop: '20px',
+            }}
+          >
+            {giveaway.description}
           </Typography>
+          <Stack mt="14px" spacing="12px">
+            <Typography sx={{ fontSize: '18px', color: '#303136' }}>
+              <>
+                <span
+                  className="giveaway-info-icon"
+                  role="img"
+                  aria-label="trophy"
+                >
+                  🏆
+                </span>{' '}
+                {giveaway.prize}
+              </>
+            </Typography>
+            <Typography sx={{ fontSize: '18px', color: '#303136' }}>
+              <>
+                <span
+                  className="giveaway-info-icon"
+                  role="img"
+                  aria-label="calendar"
+                >
+                  🗓️
+                </span>{' '}
+                {format(giveaway.endTime, 'MMMM d, yyyy')}
+              </>
+            </Typography>
+          </Stack>
         </div>
-      )}
-    </Stack>
+        {giveaway.rules && (
+          <div className="giveaway-aside-info">
+            <Typography
+              sx={{
+                fontSize: '20px',
+                fontWeight: 'bold',
+                color: '#282655',
+              }}
+            >
+              Rules
+            </Typography>
+            <Typography sx={{ fontSize: '16px', marginTop: '5px' }}>
+              {giveaway.rules}
+            </Typography>
+          </div>
+        )}
+      </Stack>
+    </>
   );
 };
 
