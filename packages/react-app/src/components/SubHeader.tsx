7e4f29ca79f804ef7b46737c0eb2fb42d3126a83
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-05-15 15:38:45 +0100

    Add back action

--- packages/react-app/src/components/SubHeader.tsx
+++ packages/react-app/src/components/SubHeader.tsx
@@ -1,9 +1,23 @@
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import { Stack, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const SubHeader = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <Stack direction="row" spacing="10px" alignItems="center" ml="60px">
+    <Stack
+      className="clickable"
+      direction="row"
+      spacing="10px"
+      alignItems="center"
+      ml="60px"
+      onClick={goBack}
+    >
       <ChevronLeftIcon sx={{ fontSize: '1.5rem' }} />
       <Typography sx={{ fontSize: '18px' }}>Back</Typography>
     </Stack>
