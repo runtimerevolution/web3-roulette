f28fdc6f88e2310cf04f9d88e4e623ac673d224b
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-05-16 12:31:03 +0100

    Setup cors

--- packages/node-app/src/app.ts
+++ packages/node-app/src/app.ts
@@ -1,14 +1,17 @@
-import express from "express";
-import { router as GiveawayRoutes } from "./routes/giveaway.route";
-import { router as LocationRoutes } from "./routes/location.routes";
+import cors from 'cors';
+import express from 'express';
+
+import { router as GiveawayRoutes } from './routes/giveaway.route';
+import { router as LocationRoutes } from './routes/location.routes';
 
 export const app = express();
 
+app.use(cors({ origin: ['http://localhost:4200'] }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({ alive: "True" });
+app.get('/', (req, res) => {
+  res.status(200).json({ alive: 'True' });
 });
 
-app.use("/giveaways", GiveawayRoutes);
-app.use("/locations", LocationRoutes);
+app.use('/giveaways', GiveawayRoutes);
+app.use('/locations', LocationRoutes);
