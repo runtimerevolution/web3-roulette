0bf725bacfaad4b83eedec319b0f3e83815c1daa
Márcia Martins <marciammart@gmail.com>
2023-05-19 15:49:26 +0100

    Remove participant in case of contract insertion failure

--- packages/node-app/src/controllers/giveaway.controller.ts
+++ packages/node-app/src/controllers/giveaway.controller.ts
@@ -150,12 +150,19 @@ export const addParticipant = async (req: Request, res: Response) => {
         .status(200)
         .json({ message: 'Participant pending manual approval' });
 
-    // add approve participant to smart contract
-    const participantHash = encrypt(participant.id);
-    await giveawaysContract.methods
-      .addParticipant(objectIdToBytes24(giveaway._id), participantHash)
-      .send({ from: process.env.OWNER_ACCOUNT_ADDRESS, gas: '1000000' });
-
+    try {
+      // add approved participant to smart contract
+      const participantHash = encrypt(participant.id);
+      await giveawaysContract.methods
+        .addParticipant(objectIdToBytes24(giveaway._id), participantHash)
+        .send({ from: process.env.OWNER_ACCOUNT_ADDRESS, gas: '1000000' });
+    } catch (error) {
+      // if contract insertion fails, remove participant from the database
+      giveaway.participants = giveaway.participants.filter((p) => p.id !== participant.id);
+      await giveaway.save();
+      throw error;
+    }
+  
     res.status(200).json({ message: 'Participant added successfully' });
   } catch (error) {
     res.status(500).json({ error: error.message });
