c9606ed2dd0473994bcd7225499f8419697afb42
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-05-22 16:17:31 +0100

    Integrate pdf download

--- packages/react-app/src/components/giveaways/AsideContent.tsx
+++ packages/react-app/src/components/giveaways/AsideContent.tsx
@@ -2,10 +2,12 @@ import { useContext } from 'react';
 import QRCode from 'react-qr-code';
 
 import { Stack, Typography } from '@mui/material';
+import ReactPDF, { PDFDownloadLink } from '@react-pdf/renderer';
 
 import { Giveaway } from '../../lib/types';
 import { GiveawayContext } from '../../pages/details';
 import Constants from '../../utils/Constants';
+import GiveawayPDFDocument from './PDFDocument';
 
 const GiveawayAsideContent = () => {
   const giveaway = useContext(GiveawayContext) as Giveaway;
@@ -26,14 +28,22 @@ const GiveawayAsideContent = () => {
           {giveaway.rules}
         </Typography>
       </div>
-      <Stack alignItems="center" mt="30px" sx={{ width: '100%' }}>
-        <div>
-          <QRCode
-            value={`${Constants.FRONTEND_URI}/giveaways/${giveaway._id}`}
-          />
-        </div>
-        <Typography variant="h6">Share Giveaway</Typography>
-      </Stack>
+      <PDFDownloadLink
+        document={<GiveawayPDFDocument {...giveaway} />}
+        fileName={`giveaway-${giveaway._id}.pdf`}
+        style={{ textDecoration: 'none' }}
+      >
+        <Stack alignItems="center" mt="30px" sx={{ width: '100%' }}>
+          <div>
+            <QRCode
+              value={`${Constants.FRONTEND_URI}/giveaways/${giveaway._id}`}
+            />
+          </div>
+          <Typography variant="h6" sx={{ color: 'black' }}>
+            Share Giveaway
+          </Typography>
+        </Stack>
+      </PDFDownloadLink>
     </div>
   );
 };
