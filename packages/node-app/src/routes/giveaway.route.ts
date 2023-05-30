a43559dc73894f755af590c11890429e6f7cc9a0
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2023-05-30 12:28:14 +0100

    Add update participant route

--- packages/node-app/src/routes/giveaway.route.ts
+++ packages/node-app/src/routes/giveaway.route.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import multer, { diskStorage } from 'multer';
+
 import {
-  listGiveaways,
-  getGiveaway,
-  createGiveaway,
-  updateGiveaway,
   addParticipant,
+  createGiveaway,
   generateWinners,
+  getGiveaway,
   getParticipants,
+  listGiveaways,
+  updateGiveaway,
+  updateParticipant,
 } from '../controllers/giveaway.controller';
 
 const storage = diskStorage({
@@ -27,5 +29,6 @@ router.post('/', upload.single('image'), createGiveaway);
 router.get('/:id', getGiveaway);
 router.put('/:id', upload.single('image'), updateGiveaway);
 router.put('/:id/participants', addParticipant);
+router.put('/:id/participants/:participantId', updateParticipant);
 router.get('/:id/participants', getParticipants);
 router.get('/:id/generate-winners', generateWinners);
