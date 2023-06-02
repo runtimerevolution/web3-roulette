c9d39f147dd23d627a8e85388199255679e9b3e3
Goncalo Adolfo <goncaloadolfo20@gmail.com>
2023-06-02 12:51:53 +0100

    Add overlay modal component

---
+++ packages/react-app/src/components/OverlayModal.tsx
@@ -0,0 +1,26 @@
+import { Button, Typography } from '@mui/material';
+import { ReactNode } from 'react';
+
+type OverlayModalProps = {
+  img: string;
+  title: string;
+  description: ReactNode;
+};
+
+const OverlayModal = ({ img, title, description }: OverlayModalProps) => {
+  return (
+    <div className="overlay">
+      <div className="modal">
+        <div style={{ textAlign: 'center' }}>
+          <img className="img" src={img} alt="Location" />
+          <Typography className="description">{description}</Typography>
+          <Button className="ok" variant="contained" disableElevation>
+            Ok
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OverlayModal;
