dc4c3c22e3d4d306b3b06d458634859205980847
Goncalo Adolfo <goncaloadolfo20@gmail.com>
2023-08-16 11:43:31 +0100

    Fix use loader data not working

--- packages/react-app/src/app/app.tsx
+++ packages/react-app/src/app/app.tsx
@@ -1,40 +1,60 @@
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import GiveawayDetailsPage, { loader as giveawayLoader } from '../pages/details';
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
+import GiveawayDetailsPage, {
+  loader as giveawayLoader,
+} from '../pages/details';
 import EditGiveaway from '../pages/edit';
 import LoginPage from '../pages/login';
 import Manage from '../pages/manage';
-import ParticipantsManagerPage, { loader as participantsLoader } from '../pages/participants';
+import ParticipantsManagerPage, {
+  loader as participantsLoader,
+} from '../pages/participants';
 import LocationEdit from '../pages/location';
 import AuthenticationProvider from '../components/login/AuthenticationProvider';
 import AuthRoute from '../routes/AuthRoute';
 import './app.module.scss';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <AuthenticationProvider>
-        <Routes>
-          <Route path="/" element={<AuthRoute />}>
-            <Route index element={<Manage />} />
-            <Route path="giveaways/new" element={<EditGiveaway />} />
-            <Route
-              path="giveaways/:giveawayId"
-              element={<GiveawayDetailsPage />}
-              loader={giveawayLoader}
-            />
-            <Route path="giveaways/:giveawayId/edit" element={<EditGiveaway />} />
-            <Route
-              path="giveaways/:giveawayId/participants"
-              element={<ParticipantsManagerPage />}
-              loader={participantsLoader}
-            />
-            <Route path="locations/new" element={<LocationEdit />} />
-          </Route>
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </AuthenticationProvider>
-    </BrowserRouter>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route
+        path="/"
+        element={
+          <AuthenticationProvider>
+            <AuthRoute />
+          </AuthenticationProvider>
+        }
+      >
+        <Route index element={<Manage />} />
+        <Route path="giveaways/new" element={<EditGiveaway />} />
+        <Route
+          path="giveaways/:giveawayId"
+          element={<GiveawayDetailsPage />}
+          loader={giveawayLoader}
+        />
+        <Route path="giveaways/:giveawayId/edit" element={<EditGiveaway />} />
+        <Route
+          path="giveaways/:giveawayId/participants"
+          element={<ParticipantsManagerPage />}
+          loader={participantsLoader}
+        />
+        <Route path="locations/new" element={<LocationEdit />} />
+      </Route>
+      <Route
+        path="/login"
+        element={
+          <AuthenticationProvider>
+            <LoginPage />
+          </AuthenticationProvider>
+        }
+      />
+    </Route>
   )
-};
+);
+const App = () => <RouterProvider router={router} />;
 
 export default App;
