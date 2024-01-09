6fe4feb71aa06ac312147b340939f69ea24d3b5e
lrosadoRevolution <l.rosado@runtime-revolution.com>
2024-01-09 13:58:04 +0000

    Update backend.ts

--- packages/react-app/src/services/backend.ts
+++ packages/react-app/src/services/backend.ts
@@ -104,6 +104,8 @@ class BackendService {
       giveaway.startTime = new Date(giveaway.startTime);
       giveaway.endTime = new Date(giveaway.endTime);
       return giveaway;
+    }else{
+      window.location.replace("/");
     }
   };
 
